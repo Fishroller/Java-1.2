5661350aa20ff7358a889788fe03654eec66e987
Владимир Ходаков <vova090666@gmail.com>
2023-05-30 21:21:32 +0300

    убраны лишние переменные

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,14 @@ public class Main {
 
         int amount; // сумма счета
         int refill;  // сумма пополнения
-        amount = 500;
-        refill = 230;
+        amount =5000;
+        refill = 2300;
         int bonus; // бонусы
         if (refill >= 1000) {
             bonus = refill / 100;
         } else {
             bonus = 0;
         }
-        int score;
-        score = amount + refill + bonus; //сумма после пополнения счета c начислением бонусов
         System.out.println("Бонус за пополнение: " + bonus);
-        System.out.println("Баланс: " + score);
     }
 }
