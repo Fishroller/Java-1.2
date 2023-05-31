8acdf865d1947b6d547af4d0e27ce61e6dde8c01
Владимир Ходаков <vova090666@gmail.com>
2023-05-31 15:19:48 +0300

    Убрано дублирование переменных

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount; // сумма счета
-        int refill;  // сумма пополнения
-        amount =5000;
-        refill = 2300;
+        int amount = 5000; // сумма счета
+        int refill = 2300;  // сумма пополнения
         int bonus; // бонусы
         if (refill >= 1000) {
             bonus = refill / 100;
